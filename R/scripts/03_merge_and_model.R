311c73dcc078d5ea372f9ab31fdeb7169ae34348
emhedlin <hedlin@ualberta.ca>
2021-07-12 21:03:22 -0500

    filling in ECCC gaps with weather station

--- R/scripts/03_merge_and_model.R
+++ R/scripts/03_merge_and_model.R
@@ -1,8 +1,6 @@
 library(tidyverse)
 library(lubridate)
 library(nuwcru)
-library(R2jags)
-library(brms)
 library(cmdstanr)
 
 # install_cmdstan()
@@ -16,12 +14,38 @@ getmode <- function(x) {
 # Load and prepare data ---------------------------------------------------
 
 
+d_surv <- read_csv("data/03_daily_survival.csv") %>% 
+  select(-X1) 
+
+# merge weather in with survival data
 dat <- do.call(rbind, 
                lapply(c(#"data/02_2013_prop.csv","data/02_2014_prop.csv",
                  "data/02_2015_prop.csv", "data/02_2016_prop.csv","data/02_2017_prop.csv"), read_csv)) %>% 
-  dplyr::select(-X1, -trt)
+  dplyr::select(-X1, -trt) %>%
+  mutate(yday = yday(date))
 
-test <- read_csv("data/02_2016_prop.csv")
+dat <- d_surv %>% 
+  select(-year) %>%
+  right_join(dat, by = c("date", "site")) %>%
+  mutate(unique_id = paste(date, ind, sep = "_")) %>%
+  filter(!duplicated(unique_id)) %>%
+  select(-unique_id)
+
+# merge in in weather station data, and replace ECCC NA's with weather station data
+dat <- do.call(rbind, 
+                      lapply(c(#"data/02_2013_prop.csv","data/02_2014_prop.csv",
+                        "data/00_weather_station_2015.csv", 
+                        "data/00_weather_station_2016.csv",
+                        "data/00_weather_station_2017.csv"), read_csv)) %>%
+  group_by(DATE) %>%
+  summarize(daily_rain_ws = sum(RAINF)) %>%
+  rename("date" = "DATE") %>%
+  right_join(dat, by = "date") %>%
+  mutate(rainfall = ifelse(is.na(precip), daily_rain_ws, precip))
+
+
+
+
 
 chicks <- read_csv("data/03a_chick_weights.csv") %>% select(-X1)
 trt <- read_csv("data/00_trt.csv") %>% 
@@ -29,33 +53,35 @@ trt <- read_csv("data/00_trt.csv") %>%
 
 
 # broodsize by n, and by weight
-brood_size <- chicks %>% 
+dat <- chicks %>% 
                 mutate(yday = yday(date)) %>%
                 group_by(site, year, yday) %>% 
                 summarize(b_size = sum(alive, na.rm = TRUE),
                           b_weight = sum(m_weight, na.rm = TRUE),
                           b_weight_var = sqrt(sum(sd_weight^2))) %>%
-  mutate(plot_date = as.Date(yday, origin = "2015-01-01"))
+  mutate(plot_date = as.Date(yday, origin = "2015-01-01")) %>%
+    # merge with nest attendance data
+  right_join(dat, by = c("site", "year", "yday")) %>%
+    # join in treatment allocations
+  left_join(trt, by = c("site", "year")) %>%
+  filter(!is.na(b_size)) 
 
 
-# merge with proportion
-dat_new <- dat %>% 
-  left_join(brood_size, by = c("site", "year", "yday"))
-
-x <- dat_new %>% 
-  left_join(trt, by = c("site", "year")) %>% 
-  filter(!is.na(b_size)) %>%
-  mutate(date = as.Date(yday, origin = paste0(year, "-01-01")))
 
 
-x_new <- x %>%
+
+
+
+
+# population weight by day/year
+x_new <- dat %>%
   group_by(yday, plot_date, year, treatment) %>%
   summarize(pop_weight = sum(b_weight)) 
 
   
 ggplot() +
-  geom_line(data = filter(x, treatment == 0), aes(x = plot_date, y = pop_weight), colour = nuwcru::grey3) +
-  geom_line(data = filter(x, treatment == 1), aes(x = plot_date, y = pop_weight), colour = nuwcru::red2) +
+  geom_line(data = filter(x_new, treatment == 0), aes(x = plot_date, y = pop_weight), colour = nuwcru::grey3) +
+  geom_line(data = filter(x_new, treatment == 1), aes(x = plot_date, y = pop_weight), colour = nuwcru::red2) +
   facet_wrap(~year, ncol = 1) +
   theme_nuwcru() + facet_nuwcru()
 
@@ -71,14 +97,12 @@ labels = tibble(
 
 
 
-# functions ---------------------------------------------------------------
-
 # model_fit ---------------------------------------------------------------
 
   # too few observations at brood size = 5
   # calculate brood age
-t <- x %>% 
-    filter(!is.na(precip)) %>%
+nonlin_dat <- dat %>% 
+    # filter(!is.na(precip)) %>%
     filter(b_size != 5) %>%
     filter(b_weight > 0) %>%
     mutate(nest_year = paste0(site, "_", year)) %>%
@@ -99,7 +123,7 @@ m <- brm(
                a ~ 1 + treatment + precip_disc + treatment:precip_disc + (1|b_size),
                b ~ 1 + treatment + precip_disc + treatment:precip_disc + (1|b_size),
                nl = TRUE),
-  data = t, 
+  data = nonlin_dat, 
   prior = c(
     prior(normal(5,1.5), nlpar = "a"),
     prior(normal(0.1,0.5), nlpar = "b")
@@ -111,7 +135,7 @@ m <- brm(
 
 
   
-pred <- t %>% 
+pred <- nonlin_dat %>% 
   expand(b_age = b_age, treatment = treatment, precip_disc = precip_disc, b_size = b_size) %>%
     tidybayes::add_fitted_draws(m, n = 100) %>%
     group_by(b_age, treatment, precip_disc, b_size) %>%
@@ -141,10 +165,10 @@ supplemented <- filter(post, treatment == 1 &
                              precip_disc %in% precip & 
                              b_size %in% clutch)
 
-raw_supplemented <- filter(t, treatment == 1 & 
+raw_supplemented <- filter(nonlin_dat, treatment == 1 & 
                              precip_disc %in% precip & 
                              b_size %in% clutch)
-raw_control <- filter(t, treatment == 0 & 
+raw_control <- filter(nonlin_dat, treatment == 0 & 
                         precip_disc %in% precip & 
                         b_size %in% clutch)
 # plot
@@ -199,29 +223,18 @@ raw_control <- filter(t, treatment == 0 &
   library(cmdstanr)
   set_cmdstan_path()
   
-  # merge weather in with survival data
-  d_weather <- d %>% 
-    select(-year) %>%
-    right_join(dat, by = c("date", "site")) %>%
-    mutate(unique_id = paste(date, ind, sep = "_")) %>%
-    filter(!duplicated(unique_id)) %>%
-    select(-unique_id)
-  
-  d <- read_csv("data/03_daily_survival.csv") %>% 
-    select(-X1) 
-  
-  obs_hist <- d %>% expand(ind = ind, age = seq(1,30,1)) %>% 
+  obs_hist <- dat %>% 
+    expand(ind = ind, age = seq(1,30,1)) %>% 
     left_join(select(d, ind, age, alive), by = c("ind", "age")) %>%
     mutate(alive = ifelse(is.na(alive), 0,alive))
   
   y <- obs_hist %>%
-    #select(-age) %>%
     pivot_wider(names_from = age, values_from = alive)
+  
+  # Observation matrix
   y_mat <- as.matrix(y[,2:ncol(y)])
+  dim(y_mat)
   
-  
-
-
 year <- obs_hist %>%
   filter(!duplicated(ind)) %>%
   mutate(year = as.numeric(str_sub(ind, -2)) - 12) %>%
@@ -230,7 +243,7 @@ year <- obs_hist %>%
 
 last <- rowSums(y_mat)
 last <- ifelse(last < 30, last+1, last)
-  
+
   stan_data <- list(
     y      = as.matrix(y[,2:ncol(y)]),
     n_ind  = nrow(y),
@@ -272,11 +285,60 @@ last <- ifelse(last < 30, last+1, last)
   
   
   
+
+# known-fate proportion ---------------------------------------------------
+
+  # some missing rain data in the ECCC historicals, so work with our weather stations
   
   
   
+  ws_weather <- do.call(rbind, 
+                 lapply(c(#"data/02_2013_prop.csv","data/02_2014_prop.csv",
+                   "data/00_weather_station_2015.csv", 
+                   "data/00_weather_station_2016.csv",
+                   "data/00_weather_station_2017.csv"), read_csv))  
+  ws_rain <- ws_weather %>% 
+    group_by(DATE) %>%
+    summarize(daily_rain_ws = sum(RAINF)) %>%
+    rename("date" = "DATE")
+  
+  combined <- d_weather %>%
+    left_join(ws_rain, by = "date") %>%
+    mutate(rainfall = ifelse(is.na(precip), daily_rain_ws, precip))
+  
+  combined[which(combined$rainfall == max(combined$rainfall)),]
+  
+  ws_weather %>% filter(DATE == as.Date("2016-07-16"))
   
   
+  # merge weather in with survival data
+  d_weather <- d %>% 
+    select(-year) %>%
+    right_join(ws_rain, by = "date") %>%
+    mutate(unique_id = paste(date, ind, sep = "_")) %>%
+    filter(!duplicated(unique_id)) %>%
+    select(-unique_id)
   
+  obs_hist <- d_weather %>% 
+    expand(ind = ind, age = seq(1,30,1)) %>% 
+    left_join(select(d_weather, ind, age, alive), by = c("ind", "age")) %>%
+    mutate(alive = ifelse(is.na(alive), 0,alive))
+  
+  
+  y <- obs_hist %>%
+    pivot_wider(names_from = age, values_from = alive)
+  
+  y_mat <- as.matrix(y[,2:ncol(y)])
+  
+  
+  #weather <- 
+  d_weather %>% 
+    expand(ind = ind, age = seq(1,30,1)) %>% 
+    left_join(select(d_weather, ind, age, precip), by = c("ind", "age")) %>%
+    pivot_wider(names_from = age, values_from = precip)
+  
+  
+  d_weather %>%
+    filter(ind == "100bk16")
   
   
